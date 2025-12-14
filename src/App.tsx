10c2177578cdee1435b204e446635330d9813fa1
unknown <bonnelye.thomas@gmail.com>
2025-12-15 00:41:37 +0100

    feat(app): update content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-function App() {
+import {
+  Container,
+  Box,
+  Snackbar,
+  Alert,
+} from '@mui/material';
+import { AppProvider, useAppContext } from './contexts/AppContext';
+import Header from './components/Header';
+import SettingsDrawer from './components/SettingsDrawer';
+import FilterPanel from './components/FilterPanel';
+import DataTable from './components/DataTable';
+
+function AppContent() {
+  const { message, messageType } = useAppContext();
+
   return (
-    <div>
-      <h1>Invader Comparator</h1>
-    </div>
+    <Box sx={{ minHeight: '100vh', bgcolor: 'background.default' }}>
+      <Header />
+      <SettingsDrawer />
+
+      <Container maxWidth="lg" sx={{ py: 4 }}>
+        <FilterPanel />
+        <DataTable />
+      </Container>
+
+      <Snackbar open={!!message} autoHideDuration={5000}>
+        <Alert severity={messageType} sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </Box>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <AppProvider>
+      <AppContent />
+    </AppProvider>
+  );
+}
